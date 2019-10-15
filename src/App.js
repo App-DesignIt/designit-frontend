f238d33ef53fd430df6ee1a9fa426f72aa44bbae
陈淇格 <cqg186495@outlook.com>
2019-10-14 18:17:16 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './components/Header.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import '../styles/header.css';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="header"></div>
+    )
+  }
+}
+
+export default Header;
